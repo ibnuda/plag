3ccb527fa9d36842631fcb25bce349394615b631
mel <teratower8@gmail.com>
2014-08-15 15:42:40 +0000

    mbenerin progres dari server

--- php/pencocokan/cobaan/mulaihitung.php
+++ php/pencocokan/cobaan/mulaihitung.php
@@ -2,12 +2,13 @@
 
 $akhir = 5000;
 session_start();
-for($i = 0; $i < $goal; $i++) {
+for($i = 0; $i < $akhir; $i++) {
     session_start();
     if(!isset($_SESSION['progres'])) {
         $_SESSION['progres'] = $i;
         $_SESSION['akhir'] = $akhir;
     }
+
     usleep(rand(500, 2000));
     $_SESSION['progres'] = $i;
     session_write_close(); 
--- php/pencocokan/cobaan/updatehitung.php
+++ php/pencocokan/cobaan/updatehitung.php
@@ -4,7 +4,9 @@ session_start();
 
 //print("anu  " . $_SESSION['progres'] . " dari " . $_SESSION['akhir'] . " baris.");
 echo '<div class="progress-bar" role="progressbar" aria-valuenow="' . 
-(( $_SESSION['progress'] + 1) * 100 / $_SESSION['goal']) . '" aria-valuemin="0" aria-valuemax="100" style="width: ' .  (( $_SESSION['progress'] + 1) * 100 / $_SESSION['goal']) . '%;"> ' + (( $_SESSION['progress']+1) * 100 / $_SESSION['goal']) . '% </div>';
-
+     (( $_SESSION['progres'] + 1) * 100 / $_SESSION['akhir']) .
+     '" aria-valuemin="0" aria-valuemax="100" style="width: ' . 
+     (( $_SESSION['progres'] + 1) * 100 / $_SESSION['akhir']) . '%;"> ' .
+     (( $_SESSION['progres'] + 1) * 100 / $_SESSION['akhir']) . '% </div>';
 
 ?>
